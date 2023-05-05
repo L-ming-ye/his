54f14a5c80549654e2345e4786758e5d0950928b
L-ming-ye <1341162191@qq.com>
2023-05-06 00:58:30 +0800

    添加用户功能 + 查看个人信息 + 退出登录

---
+++ target/classes/view/js/desktop.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+	//点击了导航栏里的按钮
+	$(".my-nav-item").click(function(){
+		bodyHides();//隐藏所有页面（div）
+		//获取导航栏里的url
+		var url = $(this).attr("url");
+		if(undefined == url || "" == url){
+			return;
+		}
+		//判断是否有对应的标签
+		if($('div[url="'+url+'"]').length>0){
+			//存在对应的标签直接显示
+			$('div[url="'+url+'"]').show();
+		}else{
+			//不存在
+			//创建一个div存当前页面
+			var divBox = '<div url="'+url+'"></div>';
+			$(".layui-body").append(divBox);
+			// $(".layui-body").children("div").attr("url",url);
+			//将指定的页面存入div
+			$('div[url="'+url+'"]').show().load(url);
+		}
+	});
+	
+	$(".my").click(function(){
+		bodyHides();
+		if($(".my-box").length>0){
+			$(".my-box").show();
+		}else{
+			var divBox = '<div class="my-box">123</div>';
+			$(".layui-body").append(divBox);
+			$(".my-box").show().load("/his/view/user/my.html");
+		}
+	});
+
+	
+	//隐藏主体的所有div
+	function bodyHides(){
+		var childrens = $(".layui-body").children().hide();
+	};
+});
---
+++ target/his/WEB-INF/classes/view/js/my.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+	//获取用户信息
+	function getUser(){
+		$.get("/his/user/get",function(e){
+			var user = e.data;
+			$(".my-name").text(user.uname);
+			$(".my-zjm").text(user.zjm);
+			$(".my-age").text(user.age);
+			$(".my-email").text(user.email);
+			$(".my-sex").text(user.sex==0?'男':user.sex==1?'女':'未知');
+			$(".my-time").text(formatTime(user.createTime));
+			
+		})
+	};
+	getUser();
+	
+	//格式化时间
+	function formatTime(time){
+		if(undefined == time || null == time){
+			return null;
+		}
+		var t = new Date(time);
+		var year = compareNum(t.getFullYear());
+		var month = compareNum(t.getMonth()+1);
+		var day = compareNum(t.getDate());
+		return year + "-" + month + "-" + day;
+	}
+	
+	//比较是否大于十
+	function compareNum(num){
+		if(num < 10){
+			return "0"+num;
+		}
+		return num;
+	}
+	
+});
